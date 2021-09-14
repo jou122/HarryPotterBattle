29183baaf206dbbe2a84c09d085d520080dd0748
jou122 <36369688+jou122@users.noreply.github.com>
2021-09-15 00:37:45 +0200

    Update HarryPotterBattle.py
    comminted xml

--- HarryPotterBattle.py
+++ HarryPotterBattle.py
@@ -29,9 +29,8 @@ def printReport(): #function to print the game report
     print ('{:<10}'.format("Health :"),'{:<10}'.format(Harry.health),'{:^30}'.format(Voldemort.health))
     print ('{:<10}'.format("Energy :"),'{:<10}'.format(Harry.energy),'{:^30}'.format(Voldemort.energy))
 
-    
-    
-    roundXml,set("Num",str(round))
+'''' 
+   
     roundXml.text="\n\n"
 
     player_1 = ET.SubElement(round, Harry.name)
@@ -46,7 +45,7 @@ def printReport(): #function to print the game report
     player_2.set('\nHealth', str(Voldemort.health))
     player_2.set('\nEnergy', str(Voldemort.energy))
     player_2.set('\nNo of shields', str(Voldemort.shields))
-
+'''
     
     
 
@@ -72,16 +71,16 @@ def battle(): # function to start the battle between the players if they have en
 
     printReport()
 
-
+'''
 
 playersXml = ET.Element('Players\n')
 
-
+'''
 
 # Creating first player
 Harry=Wizard("Harry Potter",100,500,3)
 
-
+'''
 player_1 = ET.SubElement(playersXml, '\nLight_Wizard')
 player_1.text=Harry.name
 player_1.set('\nHealth', str(Harry.health))
@@ -96,11 +95,11 @@ for spellName,spllValue in Harry.spells.items():
     spellXml.set("Power",spllValue)
     player_1_spells.append(spellXml)
 
-
+'''
 
 # Creating second player
 Voldemort=Wizard("Lord Voldemort",100,500,3)
-
+'''
 player_2 = ET.SubElement(playersXml, '\nDark_Wizard')
 player_2.text=Voldemort.name
 player_2.set('\nHealth', str(Voldemort.health))
@@ -114,7 +113,7 @@ for spellName,spllValue in Harry.spells.items():
     spellXml2.set("Name",spellName)
     spellXml2.set("Power",spllValue)
     player_2_spells.append(spellXml)
-
+'''
 
 
 
@@ -139,26 +138,25 @@ with open(abs_spells_path,'r') as file:
 Harry.sortSpells()
 Voldemort.sortSpells()
 
-
+'''
 playersText = ET.tostring(playersXml, encoding='unicode', method='xml')
-
-
-
 roundsXML=ET.Element('Rounds')
 roundsXML.text="\n"
+'''
 
 # Game
 while(True):
     flag=0
 
+    '''
     roundXml=ET.SubElement(roundsXML,'Round'+str(round))
-
+    '''
 
     # if Harry lost the game end
     if Harry.lost():
         print('{:^50}'.format("Voldemort Is The Winner"))
-        roundXml,set("Num",str(round))
-        roundXml.text="\nVoldemort Is The Winner\n"
+        '''roundXml,set("Num",str(round))
+        roundXml.text="\nVoldemort Is The Winner\n"'''
         break
         
 
@@ -166,9 +164,9 @@ while(True):
     # if voldemort lost game end
     elif Voldemort.lost():
         print('{:^50}'.format("Harry IS The Winner"))
-        roundXml.set("Num",str(round))
+        ''' roundXml.set("Num",str(round))
         roundXml.set("state","Last")
-        roundXml.text="\nHarry Is The Winner\n"
+        roundXml.text="\nHarry Is The Winner\n"'''
         break
         
     
@@ -180,8 +178,8 @@ while(True):
         Harry.currentSpell,Voldemort.currentSpell=input("Enter the two spells (Harry then Voldemort):\n").split()
     except ValueError:
         print("not valid input")
-        roundXml.set("State","invalid")
-        roundXml.text="\nnot valid input\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nnot valid input\n"'''
         continue
         flag=1
 
@@ -195,8 +193,8 @@ while(True):
     # check if the entered spell isn't in harry's spells
     if not(Harry.currentSpell in Harry.spells):
         print("spell not found in Harry's speels")
-        roundXml.set("State","invalid")
-        roundXml.text="\nspell not found in Harry's speels\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nspell not found in Harry's speels\n"'''
         continue
         flag=1
 
@@ -205,8 +203,8 @@ while(True):
     # check if the entered spell isn't in voldemort's spells
     if not(Voldemort.currentSpell in Voldemort.spells):
         print("spell not found voldemort's spells")
-        roundXml.set("State","invalid")
-        roundXml.text="\nspell not found in voldemort's speels\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nspell not found in voldemort's speels\n"'''
         continue
         flag=1
 
@@ -215,24 +213,24 @@ while(True):
     # if the player cann't attack using the current spell but still can responde
     if not (Harry.canAttack()) and Harry.canRespond():
         print("Harry can't use this spell because he don't have enough power")
-        roundXml.set("State","invalid")
-        roundXml.text="\nHarry can't use this spell because he don't have enough power\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nHarry can't use this spell because he don't have enough power\n"'''
         continue
         flag=1
     
     if not (Voldemort.canAttack()) and Voldemort.canRespond():
         print("Voldemort can't use this spell because he don't have enough power")
-        roundXml.set("State","invalid")
-        roundXml.text="\nVoldemort can't use this spell because he don't have enough power\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nVoldemort can't use this spell because he don't have enough power\n"'''
         continue
         flag=1
 
 
      # if both players cann't respond
-    if not (Voldemort.canRespond()) and not (Harry.canRespond()):
+    if (not (Voldemort.canRespond())) and (not (Harry.canRespond())):
         print("Stalemate")
-        roundXml.set("State","Stalemate")
-        roundXml.text="\nStalemate\n"
+        '''roundXml.set("State","Stalemate")
+        roundXml.text="\nStalemate\n"'''
         break
 
 
@@ -240,14 +238,14 @@ while(True):
      # if only one player cann't respond
     elif not Harry.canRespond():
         print("Harry can't responde")
-        roundXml.set("State","Stalemate")
-        roundXml.text="\nHarry can't responde\n"
+        '''roundXml.set("State","Stalemate")
+        roundXml.text="\nHarry can't responde\n"'''
         break
     
     elif not Voldemort.canRespond():
         print("voldemort can't responde")
-        roundXml.set("State","Stalemate")
-        roundXml.text="\nvoldemort can't responde\n"
+        '''roundXml.set("State","Stalemate")
+        roundXml.text="\nvoldemort can't responde\n"'''
         break
     
 
@@ -256,15 +254,15 @@ while(True):
     # if tried to apply shield  when the player has already consumed  his shields
     if Harry.currentSpell=="shield" and Harry.shields<=0:
         print("Harry can't use shields")
-        roundXml.set("State","invalid")
-        roundXml.text="\nHarry can't use shields\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nHarry can't use shields\n"'''
         continue
         flag=1
 
     if Voldemort.currentSpell=="shield" and Voldemort.shields<=0:
         print("Voldemort can't use shields")
-        roundXml.set("State","invalid")
-        roundXml.text="\nVoldemort can't use shields\n"
+        '''roundXml.set("State","invalid")
+        roundXml.text="\nVoldemort can't use shields\n"'''
         continue
         flag=1
 
@@ -274,10 +272,9 @@ while(True):
       battle()
       round += 1
       
-      roundsXML.append(roundXml)
 
-roundsXML.append(roundXml)
+'''roundsXML.append(roundXml)
 roundsText=ET.tostring(roundsXML, encoding='unicode', method='xml')
 
 with open(abs_xml_path, "w") as f:
-    f.write(playersText+"\n\n\n"+roundsText)
+    f.write(playersText+"\n\n\n"+roundsText)'''
